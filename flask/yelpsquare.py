58892a288d9c59b70bdf0d6461a57323f2d0ca58
Kevin Allen <kgallen06@gmail.com>
2014-11-25 06:59:50 -0600

    cosmetic updates

--- flask/yelpsquare.py
+++ flask/yelpsquare.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask import Markup
 from flask import render_template
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
+with app.app_context():
+    app.config['mongo_dbname']='yelpsquare'
+    mongo = PyMongo(app, config_prefix='mongo')
+    cities = mongo.db.restaurants.distinct('city')
+    cities.sort()
+
 
 @app.route('/')
 def main_page():
-    return render_template('index.html')
+  with app.app_context():
+    return render_template('index.html', cities=cities)
     
 @app.route('/search/<city>')
 def show_city(city):
-    return 'Results for %s' % city
+  return 'Results for %s' % city
     
 if __name__=='__main__':
-    app.run(debug=True)
+  app.run(debug=True)
